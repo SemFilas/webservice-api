e89350c39223abc39ff4ed634da565896846ee08
Kewilleen Gomes <kewilleen@outlook.com>
2023-05-08 22:27:46 -0300

    docker sql server initial setup

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using webservice_api.repositories;
 public class Startup 
 {
     private string serverName;
+    private string port;
     private string databaseName;
     private string user;
     private string password;
@@ -12,14 +13,15 @@ public class Startup
     public Startup(IConfiguration configuration) 
     {
         this.configuration = configuration;
-        this.serverName = this.configuration["dbms:server"];
-        this.databaseName = this.configuration["dbms:database"];
-        this.user = this.configuration["dbms:user"];
-        this.password = this.configuration["dbms:password"];
+        this.serverName = this.configuration["dbms:server"] ?? "localhost";
+        this.port = this.configuration["dbms:port"] ?? "1433";
+        this.databaseName = this.configuration["dbms:database"] ?? "SemFilas";
+        this.user = this.configuration["dbms:user"] ?? "sa";
+        this.password = this.configuration["dbms:password"] ?? "Ke@S3mfilas";
     }
 
     public void ConfigureServices(IServiceCollection services) {
-        string connection = $"Server={serverName};Database={databaseName};User Id={user};Password={password};Port=3306";
+        string connection = $"Server={serverName}, {port};Initial Catalog={databaseName};User Id={user};Password={password};TrustServerCertificate=True";
         services.AddDbContext<SemFilasContext>(options => options.UseSqlServer(connection));
         //services.AddControllers();
         //services.AddEndpointsApiExplorer();
